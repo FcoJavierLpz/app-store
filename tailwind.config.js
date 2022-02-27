6e32c7a5d27ba2a02388bcd02b4267d546dca980
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-27 17:42:10 -0600

    chore(package): add tailwindcss, postcss and autoprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html', './src/**/*.jsx']
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
