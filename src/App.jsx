7c046964886be471901ae7ba8d1e5cbc153c7db4
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-27 19:26:06 -0600

    refactor(app): add router navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <main className="container">
+          <Routes>
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/" element={<Home />} exact />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
