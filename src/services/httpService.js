7a3977e591c8bba1eabe03d4b989ee9bbcebce4a
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-27 21:39:14 -0600

    refactor(services): fix export

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { baseUrl } from '../config.json';
+import config from '../config.json';
 
 const instance = axios.create({
-  baseURL: baseUrl,
+  baseURL: config.baseUrl,
   timeout: 2500
 });
 
-return {
+export default {
   get: instance.get,
   post: instance.post,
   put: instance.put,
   delete: instance.delete
-}
+};
