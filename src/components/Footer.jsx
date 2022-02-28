dc7d8062ee99aafcbeb25944ba6959ddcbb217c0
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-28 09:54:59 -0600

    feat(footer): add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center lg:text-left px-6 pt-6">
+      <div className="text-gray-700 text-center md:mr-auto mb-6">
+        <a
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          href="mailto:fcojavierlpz91@gmail.com"
+        >
+          Send Email
+        </a>
+      </div>
+      <div className="text-gray-700 text-center p-4">© 2021 Copyright fcojavierlpz</div>
+    </footer>
+  );
+}
+
+export default Footer;
