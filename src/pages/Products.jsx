3b39f11811ac08248cd473c4ec699dadb4fdcdd9
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-27 23:25:46 -0600

    refactor(products): exchange anchor tag  for router link tag

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import http from '../services/httpService';
 
 function Products() {
@@ -20,7 +21,7 @@ function Products() {
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a href="#" className="group" key={product.id}>
+            <Link to={`/products/${product.id}`} className="group" key={product.id}>
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.image}
@@ -30,7 +31,7 @@ function Products() {
               </div>
               <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
-            </a>
+            </Link>
           ))}
         </div>
       </div>
