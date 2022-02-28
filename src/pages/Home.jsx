ae514eb34da3a8997e5053b23f8b9a594c9b967d
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-28 09:25:05 -0600

    refactor(home): add min text-size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="grid place-items-center h-96 mt-24">
-      <h1 className="text-5xl">Welcome to Fake Store</h1>
+      <h1 className="text-2xl md:text-5xl">Welcome to Fake Store</h1>
     </div>
   );
 }
