e6b13b2b4e4a850b80209d90496be6119ee1870b
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-27 23:39:49 -0600

    refactor(app): update logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 
 const NavBar = () => {
   return (
@@ -7,8 +8,7 @@ const NavBar = () => {
         <nav className="flex justify-between bg-blue-800 text-white w-screen">
           <div className="px-5 xl:px-12 py-6 flex w-full items-center">
             <Link className="text-3xl font-bold font-heading" to="/">
-              {/* <img className="h-9" src="logo.png" alt="logo"> */}
-              Fake Store
+              <img className="h-9" src={logo} alt="logo" />
             </Link>
             <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
               <li>
