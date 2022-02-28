42d3d7169f4ffcab848a2fbc560f829e8741e2b7
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-28 09:55:50 -0600

    refactor(home): remove  margin top unnecessary

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div className="grid place-items-center h-96 mt-24">
+    <div className="grid place-items-center h-96">
       <h1 className="text-2xl md:text-5xl">Welcome to Fake Store</h1>
     </div>
   );
