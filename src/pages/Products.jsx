a6ee9274f04c9a6df9277f7b6a584f470f618b96
Francisco J. Lopez <fcojavierlpz91@gmail.com>
2022-02-27 21:05:20 -0600

    refactor(products): initial values

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from 'react';
+
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(products);
+  });
+
   return <div>Products</div>;
 }
 
